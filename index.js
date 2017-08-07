1059e78f5f727f46d2621aaf05878dd746e03e25
James Koval <James.Ross.Koval@gmail.com>
2017-08-06 18:44:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// Pollyfil object.values because heroku is running old node.js
+var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+var concat = Function.bind.call(Function.call, Array.prototype.concat);
+var keys = Reflect.ownKeys;
+
+if (!Object.values) {
+	Object.values = function values(O) {
+		return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+	};
+}
+
+// Pollyfill object.keys just in case
+if (!Object.keys) Object.keys = function(o) {
+  if (o !== Object(o))
+    throw new TypeError('Object.keys called on a non-object');
+  var k=[],p;
+  for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+  return k;
+}
+
+
+
+
+
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
