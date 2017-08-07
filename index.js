f23fa1f6218318bc3ba8617de18658fa3a19c9cb
James Koval <James.Ross.Koval@gmail.com>
2017-08-06 18:15:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
