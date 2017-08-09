25b29a9ad5fc3570b697f4a7e5f3d98024e7cc32
James Koval <James.Ross.Koval@gmail.com>
2017-08-08 23:29:34 -0700

    Re-add the Object.values polyfill.

--- index.js
+++ index.js
@@ -11,10 +11,17 @@
 "use strict";
 
 // Pollyfil object.values because heroku is running old node.js
-const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
-const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
-const concat = Function.bind.call(Function.call, Array.prototype.concat);
-const keys = Reflect.ownKeys;
+var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+var concat = Function.bind.call(Function.call, Array.prototype.concat);
+var keys = Reflect.ownKeys;
+
+if (!Object.values) {
+	Object.values = function values(O) {
+		return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+	};
+}
+
 
 const express = require('express')
 const app = express();
